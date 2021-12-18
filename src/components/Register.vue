e4a6eb150c31e6a3e623aa255f0fb1853ba85fdd
sccala <sccala18@gmail.com>
2021-12-17 17:55:53 -0800

    color correction

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -57,7 +57,7 @@
                 </div>
                 <button
                     type="submit"
-                    class="w-full text-gray-100 bg-accent hover:bg-green-600 focus:ring-4 focus:green-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:hover:bg-red-700"
+                    class="w-full text-gray-100 bg-accent hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:hover:bg-green-700"
                 >
                     Register
                 </button>
